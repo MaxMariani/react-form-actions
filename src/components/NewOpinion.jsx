67a2347a7a7da273106f39207f54ba0b947b0921
Max Mariani <wmaxmariani@gmail.com>
2025-04-17 19:31:51 +0200

    Added formData validation

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,8 +1,32 @@
+import { useActionState } from "react";
+
 export function NewOpinion() {
+
+  function shareOpinionAction(formData) {
+    const title = formData.get('title');
+    const body = formData.get('body');
+    const username = formData.get('userName');
+
+    let errors = [];
+
+    if(title.trim().length < 5) {
+      errors.push('Title must be at least 5 characters long');
+    }
+
+    if(body.trim().length < 10 || body.trim().length > 300) {
+      errors.push('Opinion must be between 10 and 300 characters long');
+    }
+
+    if(!username.trim()) {
+      errors.push('Please provide your name.');
+    }
+
+  }
+
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={shareOpinionAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
